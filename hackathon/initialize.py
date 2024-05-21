c9ef85935c2eac3bcb455a63e05cfd2e29ff5407
Dorian0G <146389065+Dorian0G@users.noreply.github.com>
2024-05-20 20:18:44 -0400

    Delete hackathon directory

--- hackathon/initialize.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate(r"C:\Users\Dorian\Downloads\hackathon-98900-firebase-adminsdk-w4mr8-eb9576c9de.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
