64844df64d6742b0668ff0741978c77c6fee9427
Angel Rodriguez <ardatapro@gmail.com>
2024-05-20 20:38:22 -0400

    Home page trying to install frameworks needed

--- angel_ai/Home_Page.py
+++ angel_ai/Home_Page.py
@@ -1,3 +1,10 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("PyYAML")
 import streamlit as st
 import streamlit_authenticator as stauth
 from image_logic import rand_img_set_size
