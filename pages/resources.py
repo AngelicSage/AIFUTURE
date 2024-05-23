28709db5ac2af3efbe96501ca1b524457780afa1
Angel Rodriguez <ardatapro@gmail.com>
2024-05-23 12:47:30 -0400

    Update resources.py

--- pages/resources.py
+++ pages/resources.py
@@ -16,8 +16,17 @@ authenticator = stauth.Authenticate(
     config['pre-authorized']
 )
 authenticator.login()
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 
-   
+
+
 col1, col2, col3 = st.columns(3)
 
 # First column - GIF
